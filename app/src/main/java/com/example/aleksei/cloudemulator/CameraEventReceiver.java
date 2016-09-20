60fa30b4ac7b952dc796a58d7d856bdc2b5f159d
Lexaloris <Lexa-loris@mail.ru>
2016-09-20 23:42:20 +0300

    "Принимаем intent NEW_IMAGE"

---
+++ app/src/main/java/com/example/aleksei/cloudemulator/CameraEventReceiver.java
@@ -0,0 +1,18 @@
+package com.example.aleksei.cloudemulator;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class CameraEventReceiver extends BroadcastReceiver {
+
+    private static final String LOG_TAG = CameraEventReceiver.class.getSimpleName();
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.e(LOG_TAG, "onReveice");
+        Toast.makeText(context, "New Photo Clicked", Toast.LENGTH_LONG).show();
+    }
+}
