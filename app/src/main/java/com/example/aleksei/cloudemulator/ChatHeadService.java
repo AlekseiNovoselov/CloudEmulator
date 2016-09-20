2b68f57ff02c6fbc64e2fcd83d444fd015ef17ee
Lexaloris <Lexa-loris@mail.ru>
2016-09-21 02:28:48 +0300

    "Иконку можно двигать и удалять"

--- app/src/main/java/com/example/aleksei/cloudemulator/ChatHeadService.java
+++ app/src/main/java/com/example/aleksei/cloudemulator/ChatHeadService.java
@@ -5,21 +5,27 @@ import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.graphics.Point;
 import android.os.Build;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.os.IBinder;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 public class ChatHeadService extends Service {
 
     private RelativeLayout chatheadView, removeView;
-    private ImageView chatheadImg, removeImg;
+    private ImageView removeImg;
     private Point szWindow = new Point();
 
     private WindowManager windowManager;
+    private int x_init_cord, y_init_cord, x_init_margin, y_init_margin;
 
     @Override public IBinder onBind(Intent intent) {
         // Not used
@@ -46,7 +52,6 @@ public class ChatHeadService extends Service {
 
 
         chatheadView = (RelativeLayout) inflater.inflate(R.layout.chathead, null);
-        chatheadImg = (ImageView)chatheadView.findViewById(R.id.chathead_img);
 
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
@@ -67,6 +72,158 @@ public class ChatHeadService extends Service {
         params.x = 0;
         params.y = 100;
         windowManager.addView(chatheadView, params);
+
+        chatheadView.setOnTouchListener(new View.OnTouchListener() {
+            boolean isLongclick = false, inBounded = false;
+            int remove_img_width = 0, remove_img_height = 0;
+
+            Handler handler_longClick = new Handler();
+            Runnable runnable_longClick = new Runnable() {
+
+                @Override
+                public void run() {
+                    // TODO Auto-generated method stub
+                    Log.d(Utils.LogTag, "Into runnable_longClick");
+
+                    isLongclick = true;
+                    removeView.setVisibility(View.VISIBLE);
+                    chathead_longclick();
+                }
+            };
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                WindowManager.LayoutParams layoutParams = (WindowManager.LayoutParams) chatheadView.getLayoutParams();
+
+                int x_cord = (int) event.getRawX();
+                int y_cord = (int) event.getRawY();
+                int x_cord_Destination, y_cord_Destination;
+
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        handler_longClick.postDelayed(runnable_longClick, 600);
+
+                        remove_img_width = removeImg.getLayoutParams().width;
+                        remove_img_height = removeImg.getLayoutParams().height;
+
+                        x_init_cord = x_cord;
+                        y_init_cord = y_cord;
+
+                        x_init_margin = layoutParams.x;
+                        y_init_margin = layoutParams.y;
+
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        int x_diff_move = x_cord - x_init_cord;
+                        int y_diff_move = y_cord - y_init_cord;
+
+                        x_cord_Destination = x_init_margin + x_diff_move;
+                        y_cord_Destination = y_init_margin + y_diff_move;
+
+                        if(isLongclick){
+                            int x_bound_left = szWindow.x / 2 - (int)(remove_img_width * 1.5);
+                            int x_bound_right = szWindow.x / 2 +  (int)(remove_img_width * 1.5);
+                            int y_bound_top = szWindow.y - (int)(remove_img_height * 1.5);
+
+                            if((x_cord >= x_bound_left && x_cord <= x_bound_right) && y_cord >= y_bound_top){
+                                inBounded = true;
+
+                                int x_cord_remove = (int) ((szWindow.x - (remove_img_height * 1.5)) / 2);
+                                int y_cord_remove = (int) (szWindow.y - ((remove_img_width * 1.5) + getStatusBarHeight() ));
+
+                                if(removeImg.getLayoutParams().height == remove_img_height){
+                                    removeImg.getLayoutParams().height = (int) (remove_img_height * 1.5);
+                                    removeImg.getLayoutParams().width = (int) (remove_img_width * 1.5);
+
+                                    WindowManager.LayoutParams param_remove = (WindowManager.LayoutParams) removeView.getLayoutParams();
+                                    param_remove.x = x_cord_remove;
+                                    param_remove.y = y_cord_remove;
+
+                                    windowManager.updateViewLayout(removeView, param_remove);
+                                }
+
+                                layoutParams.x = x_cord_remove + (Math.abs(removeView.getWidth() - chatheadView.getWidth())) / 2;
+                                layoutParams.y = y_cord_remove + (Math.abs(removeView.getHeight() - chatheadView.getHeight())) / 2 ;
+
+                                windowManager.updateViewLayout(chatheadView, layoutParams);
+                                break;
+                            }else{
+                                inBounded = false;
+                                removeImg.getLayoutParams().height = remove_img_height;
+                                removeImg.getLayoutParams().width = remove_img_width;
+
+                                WindowManager.LayoutParams param_remove = (WindowManager.LayoutParams) removeView.getLayoutParams();
+                                int x_cord_remove = (szWindow.x - removeView.getWidth()) / 2;
+                                int y_cord_remove = szWindow.y - (removeView.getHeight() + getStatusBarHeight() );
+
+                                param_remove.x = x_cord_remove;
+                                param_remove.y = y_cord_remove;
+
+                                windowManager.updateViewLayout(removeView, param_remove);
+                            }
+
+                        }
+
+
+                        layoutParams.x = x_cord_Destination;
+                        layoutParams.y = y_cord_Destination;
+
+                        windowManager.updateViewLayout(chatheadView, layoutParams);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        isLongclick = false;
+                        removeView.setVisibility(View.GONE);
+                        removeImg.getLayoutParams().height = remove_img_height;
+                        removeImg.getLayoutParams().width = remove_img_width;
+                        handler_longClick.removeCallbacks(runnable_longClick);
+
+                        if(inBounded){
+                            stopService(new Intent(ChatHeadService.this, ChatHeadService.class));
+                            inBounded = false;
+                            break;
+                        }
+
+                        int y_diff = y_cord - y_init_cord;
+
+                        y_cord_Destination = y_init_margin + y_diff;
+
+                        int BarHeight =  getStatusBarHeight();
+                        if (y_cord_Destination < 0) {
+                            y_cord_Destination = 0;
+                        } else if (y_cord_Destination + (chatheadView.getHeight() + BarHeight) > szWindow.y) {
+                            y_cord_Destination = szWindow.y - (chatheadView.getHeight() + BarHeight );
+                        }
+                        layoutParams.y = y_cord_Destination;
+
+                        inBounded = false;
+
+                        break;
+                    default:
+                        Log.d(Utils.LogTag, "chatheadView.setOnTouchListener  -> event.getAction() : default");
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    private int getStatusBarHeight() {
+        int statusBarHeight = (int) Math.ceil(25 * getApplicationContext().getResources().getDisplayMetrics().density);
+        return statusBarHeight;
+    }
+
+
+    private void chathead_longclick(){
+        Log.d(Utils.LogTag, "Into ChatHeadService.chathead_longclick() ");
+
+        WindowManager.LayoutParams param_remove = (WindowManager.LayoutParams) removeView.getLayoutParams();
+        int x_cord_remove = (szWindow.x - removeView.getWidth()) / 2;
+        int y_cord_remove = szWindow.y - (removeView.getHeight() + getStatusBarHeight() );
+
+        param_remove.x = x_cord_remove;
+        param_remove.y = y_cord_remove;
+
+        windowManager.updateViewLayout(removeView, param_remove);
     }
 
     @Override
